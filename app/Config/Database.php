422bd39ce1468a6a7e273efe7e9dfdc6d382413e
yUm1k4 <zaiabdullah99@gmail.com>
2021-07-23 13:11:52 +0700

    feat: nggk tau config apa nih

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -1,4 +1,6 @@
-<?php namespace Config;
+<?php
+
+namespace Config;
 
 /**
  * Database Configuration
@@ -88,20 +90,16 @@ class Database extends \CodeIgniter\Database\Config
 		// Ensure that we always set the database group to 'tests' if
 		// we are currently running an automated test suite, so that
 		// we don't overwrite live data on accident.
-		if (ENVIRONMENT === 'testing')
-		{
+		if (ENVIRONMENT === 'testing') {
 			$this->defaultGroup = 'tests';
 
 			// Under Travis-CI, we can set an ENV var named 'DB_GROUP'
 			// so that we can test against multiple databases.
-			if ($group = getenv('DB'))
-			{
-				if (is_file(TESTPATH . 'travis/Database.php'))
-				{
+			if ($group = getenv('DB')) {
+				if (is_file(TESTPATH . 'travis/Database.php')) {
 					require TESTPATH . 'travis/Database.php';
 
-					if (! empty($dbconfig) && array_key_exists($group, $dbconfig))
-					{
+					if (!empty($dbconfig) && array_key_exists($group, $dbconfig)) {
 						$this->tests = $dbconfig[$group];
 					}
 				}
